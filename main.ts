1a47ed72ee5bba70fb534edada02e07519e573b9
jerry22lin <110014366+jerry22lin@users.noreply.github.com>
2022-07-26 15:26:33 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 basic.forever(function () {
     basic.showNumber(input.compassHeading())
+    if (input.compassHeading() <= 5 || input.compassHeading() >= 355) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
 })
