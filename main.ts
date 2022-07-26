569309ba518d5ea41d4ae17eac8fc7d9d749aee1
jerry22lin <110014366+jerry22lin@users.noreply.github.com>
2022-07-26 15:39:13 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,17 +18,17 @@ basic.forever(function () {
     } else if (input.compassHeading() <= 275 && input.compassHeading() >= 265) {
         basic.showLeds(`
             . . # . .
-            . # . . .
+            . . . # .
             # # # # #
-            . # . . .
+            . . . # .
             . . # . .
             `)
     } else if (input.compassHeading() <= 95 && input.compassHeading() >= 85) {
         basic.showLeds(`
             . . # . .
-            . . . # .
+            . # . . .
             # # # # #
-            . . . # .
+            . # . . .
             . . # . .
             `)
     } else {
